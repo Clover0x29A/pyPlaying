45b4187785718c2110b244bbdc30478a0be8ea39
Clover0x29A <85973140+Clover0x29A@users.noreply.github.com>
2021-08-30 23:02:39 -0700

    yup still playing

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 
 import turtle
 
-def draw_square(t, input_angle):
+def draw_square(t, input_angle, angle):
     degree = 90 + input_angle
-    t.fillcolor('orange')
+    if angle == 0:
+        angle = 1
+    my_red = angle * input_angle + 40
+    my_green = (my_red % 2) * 2
+    my_blue = my_red % angle
+    if my_red > 255:
+        my_red = 244
+    if my_green > 255:
+        my_green = 255
+    if my_blue < 0:
+        my_blue = 155
+    t.color((my_red,my_green, my_blue))
     t.begin_fill()
-    #t.fd(100)
     t.left(degree)
     t.fd(100)
     t.left(90)
@@ -17,11 +27,12 @@ def draw_square(t, input_angle):
     t.end_fill()
 
 s = turtle.getscreen()
+s.colormode(255)
 t = turtle.Turtle()
-t.speed(1)
+t.speed(5)
 
-for angle in range(0,5):
-    draw_square(t, 15 * angle)
+for angle in range(0,19):
+    draw_square(t, 15, angle)
 turtle.exitonclick()
 
 
